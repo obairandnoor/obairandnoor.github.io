c42a0676eb90bb6c9ad79226080a63a3aa9120f6
Hana Habib <hanahabib@HTQ-MBPRO.isri.cmu.edu>
2017-03-12 18:06:24 -0400

    fixed countdown time

--- js/forms.js
+++ js/forms.js
@@ -1,10 +1,14 @@
 // Lantern Wedding Theme 
 // Version : v1.0
-
+Email = { send: function (t, e, o, n, d, r, c) { var a = Math.floor(1e6 * Math.random() + 1), m = "http://smtpjs.com/smtp.aspx?"; m += "From=" + t, m += "&to=" + e, m += "&Subject=" + encodeURIComponent(o), m += "&Body=" + encodeURIComponent(n), void 0 == d.token ? (m += "&Host=" + d, m += "&Username=" + r, m += "&Password=" + c, m += "&Action=Send") : (m += "&SecureToken=" + d.token, m += "&Action=SendFromStored"), m += "&cachebuster=" + a, Email.addScript(m) }, addScript: function (t) { var e = document.createElement("link"); e.setAttribute("rel", "stylesheet"), e.setAttribute("type", "text/xml"), e.setAttribute("href", t), document.body.appendChild(e) } };
 function validate_email(email) 
 {
    var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
    return reg.test(email);
+}
+function email_rsvp(name, events, party, children)
+{
+
 }
 $(document).ready(function(e) {
     $('#contact-submit').click(function(e) {
@@ -160,25 +164,25 @@ function rsvp_submit()
 			$('#rsvp-name').addClass('error');
 			submit_flag = 'no';
 	}
-	var rsvp_email =$('#rsvp-email').val();
+	var rsvp_email =$('#rsvp-event').val();
 	if(rsvp_email =='')
 	{
 			if(submit_flag == 'yes')
 			{
-				$('#rsvp-email').focus();
+				$('#rsvp-event').focus();
 			}
-			$('#rsvp-email').addClass('error');
+			$('#rsvp-event').addClass('error');
 			submit_flag = 'no';
 	}
-	if(!validate_email(rsvp_email))
-	{
-			if(submit_flag == 'yes')
-			{
-				$('#rsvp-email').focus();
-			}
-			$('#rsvp-email').addClass('error');
-			submit_flag = 'no';
-	}
+	// if(!validate_email(rsvp_email))
+	// {
+	// 		if(submit_flag == 'yes')
+	// 		{
+	// 			$('#rsvp-event').focus();
+	// 		}
+	// 		$('#rsvp-event').addClass('error');
+	// 		submit_flag = 'no';
+	// }
 	var rsvp_persons = $('#rsvp-persons').val();
 	if($('#rsvp-persons').val() =='')
 	{
@@ -189,19 +193,20 @@ function rsvp_submit()
 			$('#rsvp-persons').addClass('error');
 			submit_flag = 'no';
 	}
-	var rsvp_wedding = $('#rsvp-wedding').val()
-	if( rsvp_wedding =='')
-	{
-			if(submit_flag == 'yes')
-			{
-				$('#rsvp-wedding').focus();
-			}
-			$('#rsvp-wedding').addClass('error');
-			submit_flag = 'no';
-	}
+	var rsvp_children = $('#rsvp-children').val();
+	// var rsvp_wedding = $('#rsvp-wedding').val()
+	// if( rsvp_wedding =='')
+	// {
+	// 		if(submit_flag == 'yes')
+	// 		{
+	// 			$('#rsvp-wedding').focus();
+	// 		}
+	// 		$('#rsvp-wedding').addClass('error');
+	// 		submit_flag = 'no';
+	// }
 	if(submit_flag != 'yes')
 	{	
-		$('#rsvp-error').show().html('Please review the above details filled');
+		$('#rsvp-error').show().html('Please review the above details.');
 	}
 	else
 	{
@@ -210,12 +215,15 @@ function rsvp_submit()
 		$('#rsvp-failed').hide();
 		$('#rsvp-form').hide();
 		$('#rsvp-loading').show();
+		var subject = "RSVP from " + rsvp_name; 
+		var body = "Event(s) attending: " + rsvp_email + "\nNumber of people over age 3: " + rsvp_persons + "\nNumber of children under 3: " + rsvp_children;
+		Email.send("obairwedsnoor@gmail.com","obairwedsnoor@gmail.com",subject,body,"smtp.gmail.com","obairwedsnoor@gmail.com","Wedding17");
 
 		$.ajax({
 			url: 'rsvp.php',
 			type: 'post',
 			cache: false,
-			data: {'name' : rsvp_name, 'email' : rsvp_email , 'persons' : rsvp_persons, 'wedding' : rsvp_wedding},
+			data: {'name' : rsvp_name, 'email' : rsvp_email , 'persons' : rsvp_persons},
 			success: function(data) {
 							if(data =='ok')
 							{
